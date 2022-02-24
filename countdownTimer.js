1f7e3727da8022a8775cb6c5cfc05ca682646566
Bill Wu <billwu93@gmail.com>
2022-02-24 17:49:24 +1300

    Edited JS, working CD timer complete, with minor issues.

--- countdownTimer.js
+++ countdownTimer.js
@@ -1,39 +1,80 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Mar 15, 2022 15:37:25").getTime();
+let timer = "5:00"
+document.getElementById("textfield").innerHTML = timer;
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+var x = false;
+function countDown () {
+ 
+  let currenttime = document.getElementsByTagName('p')[0].textContent;
+  let countdowntime = currenttime.split(":");
+ 
+  let seconds = countdowntime.pop();
+  let minutes = countdowntime.pop();
+  let hours = countdowntime.pop();
+  let days = countdowntime.pop();
 
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + ":" + hours + ":"
-  + minutes + ":" + seconds;
-
-  // If the count down is over, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+  seconds--;
+ 
+  if (seconds == -1){
+    seconds += 60;
+    minutes --;
   }
-}, 1000);
 
-function plusHour(){
-  hours ++;
-  document.getElementById("demo").innerHTML = "MyASS"
-  //days + "d " + hours + "h "
-  //+ minutes + "m " + seconds + "s ";
+  if (minutes == -1){
+    minutes += 60;
+    hours --;
+  }
+
+  if (hours == -1){
+    hours += 24;
+    days --;
+  }  
+
+  if (days == undefined) {
+    if (hours == undefined) {
+      if (minutes == undefined) {
+        document.getElementById("textfield").innerHTML = seconds;
+        zero(days, hours, minutes, seconds);
+        return;
+      }
+      document.getElementById("textfield").innerHTML = minutes + ":" + seconds;
+      zero(days, hours, minutes, seconds);
+      return;
+    }
+    document.getElementById("textfield").innerHTML = hours + ":"
+  + minutes + ":" + seconds;
+    zero(days, hours, minutes, seconds);
+    return;
+  }
+  
+  document.getElementById("textfield").innerHTML = days + ":" + hours + ":"
+  + minutes + ":" + seconds;
+  zero(days, hours, minutes, seconds);
+
 }
-function changeColor(newColor) {
-  var elem = document.getElementById('para');
-  elem.style.color = newColor;
+
+function zero (d, h, m, s) {
+  if ((d == 0 || d == undefined) && (h == 0 || h == undefined) && (m == 0 || m == undefined)  && (s == 0 || s == undefined))  {
+    alert ("Time's Up");
+    clearInterval(x);
+  }
 }
+
+function startStop () {
+  if (x == false) {
+    timer = document.getElementsByTagName('p')[0].textContent;
+    x = setInterval(countDown, 1000);
+  } else {
+    clearInterval(x);
+    x = false;
+  }
+}
+
+function reset () {
+  if (x == false) {
+    document.getElementById("textfield").innerHTML = timer;
+  } else {
+    clearInterval(x);
+    document.getElementById("textfield").innerHTML = timer;
+  }
+}
+
